a713d4b30f24e6c1f7c96845fcecf45e7403e64a
Andrew Totmin <andrew.totmin@gmail.com>
2025-12-04 20:39:12 +0500

    Feat: add validate function

--- src/app.js
+++ src/app.js
@@ -1,5 +1,48 @@
 // import './style.css'
-import onChange from 'on-change'
+// import onChange from 'on-change'
+import * as yup from 'yup'
 
 const form = document.querySelector('.rss-form')
-const input = document.querySelector('#url-input') 
+const input = document.querySelector('#url-input')
+const buttonSubmit = document.querySelector('button[type=submit]')
+
+const validate = (url, urls) => {
+  const schema = yup.object({
+    url: yup.string().required().url().notOneOf(urls),
+  })
+  return schema
+    .validate({ url: url })
+    .then(() => null)
+    .catch((e) => e.message)
+}
+
+const state = {
+  form: {
+    isValid: null,
+    error: null,
+  },
+  feeds: [],
+  posts: [],
+  // loadingState: {
+  //   status: 'idle',
+  //   errors: null,
+  // },
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const formData = new FormData(e.target)
+  const url = formData.get('url').trim()
+  const existingUrls = state.feeds.map((feed) => feed.url)
+
+  validate(url, existingUrls)
+    .then((error) => {
+      if (error) {
+        state.form = { isValid: false, error: error }
+        return
+      } else {
+        state.form = { isValid: true, error: null }
+      }
+    })
+})
