4ee6d8a25beefb543e8d50f608f52b2f340e778e
Andrew Totmin <andrew.totmin@gmail.com>
2025-12-12 18:42:44 +0500

    chore(ru.js): separate localization strings semantically

--- src/locales/ru.js
+++ src/locales/ru.js
@@ -1,16 +1,22 @@
 export default {
   translation: {
-    errors: {
+    validation: {
       required: 'Поле не должно быть пустым',
       url: 'Ссылка должна быть валидным URL',
       notOneOf: 'RSS уже существует',
     },
-    elements: {
-      feeds: 'Фиды',
-      Posts: 'Посты',
+    loadingErrors: {
+      invalidRSS: 'Ресурс по этой ссылке не является корректным RSS-фидом',
+      networkError: 'Проблемы с сетью. Не удалось подключиться к серверу',
+      unknownError: 'Произошла непредвиденная ошибка приложения',
     },
     status: {
       success: 'RSS успешно загружен',
+      loading: 'Загрузка...',
+    },
+    elements: {
+      feeds: 'Фиды',
+      Posts: 'Посты',
     },
   },
 }
