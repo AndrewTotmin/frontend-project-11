dfbc3c7dcecc3fcacecd01ffe1323e69e7aff531
Andrew Totmin <andrew.totmin@gmail.com>
2025-12-13 20:11:19 +0500

    feat: implement the feed loading function

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 import * as yup from 'yup'
+import axios from 'axios'
 import i18n from 'i18next'
-import resources from './locales/index'
+import _ from 'lodash'
 import onChange from 'on-change'
-import { renderError } from './view'
+
+import { LOADING_ERRORS, STATUS, VALIDATION_ERRORS } from './constants'
+import resources from './locales/index'
+import { renderError, renderLoadingProcces } from './view'
 
 const i18nInstance = i18n.createInstance()
 
@@ -15,11 +19,11 @@ i18nInstance
   .then(() => {
     const setLocaleObj = {
       string: {
-        url: 'errors.url',
+        url: VALIDATION_ERRORS.URL,
       },
       mixed: {
-        required: 'errors.required',
-        notOneOf: 'errors.notOneOf',
+        required: VALIDATION_ERRORS.REQUIRED,
+        notOneOf: VALIDATION_ERRORS.NOT_ONE_OF,
       },
     }
 
@@ -40,17 +44,20 @@ i18nInstance
         isValid: true,
         error: null,
       },
+      loadingProcces: {
+        status: STATUS.IDLE, // loading, success, error
+        error: null,
+      },
       feeds: [],
       posts: [],
-      loadingProcces: {
-        status: 'idle', // loading, success, error
-        errors: null,
-      },
     }
 
     const handleChange = (path) => {
       if (path.startsWith('form')) {
         renderError(watchedState, elements, i18nInstance)
+      }
+      else if (path.startsWith('loadingProcces')) {
+        renderLoadingProcces(watchedState, elements, i18nInstance)
       }
       else {
         throw new Error(`Unknown path ${path}`)
@@ -66,9 +73,89 @@ i18nInstance
 
     const watchedState = onChange(initialState, handleChange)
 
-    // const loadData = (url) => {
-    //   watchedState.loadingProcces.status = 'loading'
-    // }
+    const parseData = (data) => {
+      const parser = new DOMParser()
+      return parser.parseFromString(data, 'application/xml')
+    }
+
+    const getProxyUrl = (url) => {
+      const allOrigins = 'https://allorigins.hexlet.app/get'
+      const proxyBaseUrl = new URL(allOrigins)
+      proxyBaseUrl.searchParams.set('disableCache', 'true')
+      proxyBaseUrl.searchParams.set('url', url)
+      return proxyBaseUrl.toString()
+    }
+
+    const extractData = (data, url) => {
+      const parsedData = parseData(data)
+
+      if (parsedData.querySelector('parsererror')) {
+        throw new Error(LOADING_ERRORS.INVALID_RSS)
+      }
+
+      const feedTitle = parsedData.querySelector('channel > title').textContent
+      const feedDescription = parsedData.querySelector(
+        'channel > description',
+      ).textContent
+      const feed = { title: feedTitle, description: feedDescription, url: url }
+
+      const itemElements = Array.from(
+        parsedData.querySelectorAll('channel > item'),
+      )
+      const posts = itemElements.map((post) => {
+        return {
+          title: post.querySelector('title').textContent,
+          description: post.querySelector('description').textContent,
+          url: post.querySelector('link').textContent,
+        }
+      })
+      return { feed, posts }
+    }
+
+    const handleLoadingError = (error, watchedState) => {
+      console.log(error)
+
+      let errorCode
+
+      if (axios.isAxiosError(error)) {
+        errorCode = LOADING_ERRORS.NETWORK_ERROR
+      }
+      else if (error.message === LOADING_ERRORS.INVALID_RSS) {
+        errorCode = LOADING_ERRORS.INVALID_RSS
+      }
+      else {
+        errorCode = LOADING_ERRORS.UNKNOWN_ERROR
+      }
+      watchedState.loadingProcces = { status: STATUS.ERROR, error: errorCode }
+    }
+
+    const loadData = (url) => {
+      watchedState.loadingProcces.status = STATUS.LOADING
+
+      const proxyUrl = getProxyUrl(url)
+
+      axios
+        .get(proxyUrl)
+        .then((response) => {
+          const { feed, posts } = extractData(response.data.contents, url)
+
+          feed.id = _.uniqueId('feed_')
+          watchedState.feeds.push(feed)
+
+          const relatedPosts = posts.map((post) => {
+            return {
+              ...post,
+              id: _.uniqueId('post_'),
+              feedId: feed.id,
+            }
+          })
+          watchedState.posts.push(...relatedPosts)
+          watchedState.loadingProcces.status = STATUS.SUCCESS
+        })
+        .catch((error) => {
+          handleLoadingError(error, watchedState)
+        })
+    }
 
     elements.form.addEventListener('submit', (e) => {
       e.preventDefault()
@@ -84,9 +171,9 @@ i18nInstance
         }
         else {
           watchedState.form = { isValid: true, error: null }
-          // loadData(url)
-          // elements.form.reset()
-          // elements.input.focus()
+          loadData(url)
+          elements.form.reset()
+          elements.input.focus()
         }
       })
     })
