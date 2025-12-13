224c3d47b1bb54a3a3d9e71f49fc698fc7027e29
Andrew Totmin <andrew.totmin@gmail.com>
2025-12-13 20:12:52 +0500

    feat(view.js): implement loading status display

--- src/view.js
+++ src/view.js
@@ -1,3 +1,5 @@
+import { APP_MESSAGES, STATUS } from './constants'
+
 const renderError = (state, elements, i18nInstance) => {
   if (state.form.isValid) {
     elements.input.classList.remove('is-invalid')
@@ -9,6 +11,34 @@ const renderError = (state, elements, i18nInstance) => {
   }
 }
 
-export {
-  renderError,
+const renderLoadingProcces = (state, elements, i18nInstance) => {
+  elements.feedback.classList.remove('text-danger', 'text-success', 'text-info')
+  switch (state.loadingProcces.status) {
+    case STATUS.LOADING:
+      elements.submitButton.disabled = true
+      elements.feedback.classList.add('text-info')
+      elements.feedback.textContent = i18nInstance.t(APP_MESSAGES.STATUS_LOADING)
+      break
+
+    case STATUS.SUCCESS:
+      elements.submitButton.disabled = false
+      elements.feedback.classList.add('text-success')
+      elements.feedback.textContent = i18nInstance.t(APP_MESSAGES.STATUS_SUCCESS)
+      break
+
+    case STATUS.ERROR:
+      elements.submitButton.disabled = false
+      elements.feedback.classList.add('text-danger')
+      elements.feedback.textContent = i18nInstance.t(state.loadingProcces.error)
+      break
+
+    case STATUS.IDLE:
+      elements.submitButton.disabled = false
+      break
+
+    default:
+      throw new Error(`Unknown status: ${state.loadingProcces.status}`)
+  }
 }
+
+export { renderError, renderLoadingProcces }
