d33c41ab53a49c7f07fbb7ec94365b0adb9938e5
Andrew Totmin <andrew.totmin@gmail.com>
2025-12-15 11:09:02 +0500

    fix(view.js): fix: Correctly display validation errors with text-danger class

--- src/view.js
+++ src/view.js
@@ -1,12 +1,14 @@
 import { APP_MESSAGES, STATUS } from './constants'
 
 const renderError = (state, elements, i18nInstance) => {
+  elements.feedback.classList.remove('text-danger', 'text-success', 'text-info')
   if (state.form.isValid) {
     elements.input.classList.remove('is-invalid')
     elements.feedback.textContent = ''
   }
   else {
     elements.input.classList.add('is-invalid')
+    elements.feedback.classList.add('text-danger')
     elements.feedback.textContent = i18nInstance.t(state.form.error)
   }
 }
